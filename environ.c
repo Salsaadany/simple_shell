53ff010f97a8f8a20bbf874b07b00691a94b9295
Sherweet Alsaadany <122960919+Salsaadany@users.noreply.github.com>
2023-04-25 01:51:05 +0200

    Errors fixed

--- environ.c
+++ environ.c
@@ -8,7 +8,7 @@
  */
 int myenv_maati(info_t *info)
 {
-	print_list_str(info->env);
+	p_lst(info->env);
 	return (0);
 }
 
@@ -26,7 +26,7 @@ char *getenv_maati(info_t *info, const char *name)
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
+		p = stw_sherry(node->str, name);
 		if (p && *p)
 			return (p);
 		node = node->next;
@@ -45,10 +45,10 @@ int mysetenv_maati(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		eputs_maati("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (setenv_maati(info, info->argv[1], info->argv[2]))
+	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
@@ -65,7 +65,7 @@ int _myunsetenv(info_t *info)
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		eputs_maati("Too few arguements.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
@@ -86,7 +86,7 @@ int populate_env_list(info_t *info)
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
+		a_nnd(&node, environ[i], 0);
 	info->env = node;
 	return (0);
 }
