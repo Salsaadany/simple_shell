d3a5c531666f0d8c72c99b48de7b3d3f9fdb1219
Sherweet Alsaadany <122960919+Salsaadany@users.noreply.github.com>
2023-04-25 22:09:15 +0200

    Fixed

--- shell.h
+++ shell.h
@@ -94,8 +94,8 @@ typedef struct passinfo
 	int histcount;
 } info_t;
 
-#define INFO_INIT                                                               \
-	{                                                                           \
+#define INFO_INIT
+	{ 
 		NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 			0, 0, 0                                                             \
 	}
