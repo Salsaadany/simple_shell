4b32a0c1673eee3b867d292ddc9b035ad5288816
Sherweet Alsaadany <122960919+Salsaadany@users.noreply.github.com>
2023-04-25 02:12:35 +0200

    Updates

--- getenv.c
+++ getenv.c
@@ -10,7 +10,7 @@ char **get_environ_maati(info_t *info)
 {
 	if (!info->environ || info->env_changed)
 	{
-		info->environ = list_to_strings(info->env);
+		info->environ = l_str(info->env);
 		info->env_changed = 0;
 	}
 
@@ -35,10 +35,10 @@ int _unsetenv(info_t *info, char *var)
 
 	while (node)
 	{
-		p = starts_with(node->str, var);
+		p = stw_sherry(node->str, var);
 		if (p && *p == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
+			info->env_changed = dl_nd(&(info->env), i);
 			i = 0;
 			node = info->env;
 			continue;
@@ -67,16 +67,16 @@ int _setenv(info_t *info, char *var, char *value)
 	if (!var || !value)
 		return (0);
 
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	buf = malloc(s_lngth(var) + s_lngth(value) + 2);
 	if (!buf)
 		return (1);
-	_strcpy(buf, var);
-	_strcat(buf, "=");
-	_strcat(buf, value);
+	strncpy_maati(buf, var);
+	st_conc(buf, "=");
+	st_conc(buf, value);
 	node = info->env;
 	while (node)
 	{
-		p = starts_with(node->str, var);
+		p = stw_sherry(node->str, var);
 		if (p && *p == '=')
 		{
 			free(node->str);
@@ -86,7 +86,7 @@ int _setenv(info_t *info, char *var, char *value)
 		}
 		node = node->next;
 	}
-	add_node_end(&(info->env), buf, 0);
+	a_nnd(&(info->env), buf, 0);
 	free(buf);
 	info->env_changed = 1;
 	return (0);
