cd8c4ca9e652ac59363be2d69e2cacc1ecc9fdcb
Sherweet Alsaadany <122960919+Salsaadany@users.noreply.github.com>
2023-04-25 02:18:11 +0200

    Updates

--- getinfo.c
+++ getinfo.c
@@ -24,14 +24,14 @@ void set_information(info_t *info, char **av)
 	info->fname = av[0];
 	if (info->arg)
 	{
-		info->argv = strtow(info->arg, " \t");
+		info->argv = sp_rep(info->arg, " \t");
 		if (!info->argv)
 		{
 
 			info->argv = malloc(sizeof(char *) * 2);
 			if (info->argv)
 			{
-				info->argv[0] = _strdup(info->arg);
+				info->argv[0] = st_dpl(info->arg);
 				info->argv[1] = NULL;
 			}
 		}
@@ -39,8 +39,8 @@ void set_information(info_t *info, char **av)
 			;
 		info->argc = i;
 
-		replace_alias(info);
-		replace_vars(info);
+		al_rep(info);
+		ve_rep(info);
 	}
 }
 
@@ -51,7 +51,7 @@ void set_information(info_t *info, char **av)
  */
 void free_information(info_t *info, int all)
 {
-	ffree(info->argv);
+	f_mp(info->argv);
 	info->argv = NULL;
 	info->path = NULL;
 	if (all)
@@ -59,14 +59,14 @@ void free_information(info_t *info, int all)
 		if (!info->cmd_buf)
 			free(info->arg);
 		if (info->env)
-			free_list(&(info->env));
+			x_lst(&(info->env));
 		if (info->history)
-			free_list(&(info->history));
+			x_lst(&(info->history));
 		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
+			x_lst(&(info->alias));
+		f_mp(info->environ);
 			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
+		f_brdm((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
 		_putchar(BUF_FLUSH);
